8a688a9347a94926bb23eb00da5990870b5b761c
Lucas Rolim <lucasrolim@poli.ufrj.br>
2017-08-19 14:05:24 -0300

    Assigment 1 done
    Move edges now is possible

--- assignment_1/sketch.js
+++ assignment_1/sketch.js
@@ -1,53 +1,70 @@
 var lines;
 var newLine;
 var lock;
-var selectPoint;
+var selectedPoint;
+var intersections;
+
 function setup(){
     createCanvas(400,400);
     lines = [];
+    intersections = 0;
     newLine = {point1:[],point2:[]};
     lock = true;
-    selectPoint = false;
+    selectedPoint = {status:false,lineIndex:null,point: null};
 }
 
 function draw(){
+    intersections = 0;
     background(0);
     stroke(153);
     if(!lock){
         line(newLine.point1[0],newLine.point1[1],mouseX,mouseY);
     }
-    for(var i = 0; i < lines.length; i++){
+    for(i = 0; i < lines.length; i++){
         line(lines[i].point1[0],lines[i].point1[1],lines[i].point2[0],lines[i].point2[1])
     }
-    for(var i = 0; i < lines.length; i++){
-        for(var j = 0; j < lines.length; j++){
+    for(i = 0; i < lines.length; i++){
+        for(j = 0; j < lines.length; j++){
             if(i != j){
                 line1 = [{x:lines[i].point1[0], y:lines[i].point1[1]},{x:lines[i].point2[0],y:lines[i].point2[1]}]
                 line2 = [{x:lines[j].point1[0], y:lines[j].point1[1]},{x:lines[j].point2[0],y:lines[j].point2[1]}]
                 var intersec = line_intersect(line1,line2);
                 if(intersec != null){
-                    ellipse(intersec.x,intersec.y,5,5);
+                    intersections += 1;
+                    // sinaliza interseções das linhas
+                    fill(134);
+                    ellipse(intersec.x,intersec.y,8,8);
                 }
             }
         }
     }
-    //ellipse(100,100,10,10);
     
 }
 
 
 // When the user clicks the mouse
 function mousePressed() {
-    if(selectPoint){
-        console.log("TODO");
+    console.log(intersections);
+    // verifica se o usuário clicou em algum ponto próximo a uma extremidade de linah existente
+    nearPoint(mouseX,mouseY);
+    if(selectedPoint.status && lock === true){
+        // modifica a extremidade da linha selecionada
+        var baseLine = [lines[selectedPoint.lineIndex].point2,lines[selectedPoint.lineIndex].point1];
+        lines.splice(selectedPoint.lineIndex,1);
+        newLine.point1 = baseLine[selectedPoint.point-1];
+        selectedPoint.status =false;
+        lock = false;
     }
     else{
-         if(lock){
+        if(lock){
+            // começa a desenhar uma nova linha com o ponto clicado
+            // o outro ponto da linha será a localização do mouse no momento
             newLine.point1.push(mouseX);
             newLine.point1.push(mouseY);
             lock = false;
         }
         else{
+            // termina de desenhar uma linha a qual já se sabe um ponto
             newLine.point2.push(mouseX);
             newLine.point2.push(mouseY);
             lines.push(newLine);
@@ -58,29 +75,50 @@ function mousePressed() {
 
 }
 
-// range on click
+// Calcula se uma das extremidades de alguma das linhas está próxima ao ponto clicado pelo mouse
+// Manipula o objeto selectedPoint:
+// status: false se não existe nenhum ponto perto e true se existe
+// lineIndex: Index da linha a qual o ponto pertence no array lines
+// point: indica qual exteminado da linha mencionado vamos querer alterar
 
-//function clickRange(m_x,m_y,x,y){
-//    var dist = sqrt(Math.pow(m_x - x) + Math.pow(m_y - y))
-//    if(dist < 20){
-//       return dist;
-//    }
-//    else{
-//        return -1;
-//    }
-//}
-//
-//function checkMinimunDist(m_x,m_y){
-//    var point = {index:-1,dist:-1};
-//    var dist = clickRange(m_x,m_y,lines[0].point1[0],lines[0].point1[1]);
-//    for(var i = 0; i < lines.length; i++){
-//        if(clickRange(m_x,m_y,lines[i].point1[0],lines[i].point1[1])){
-//        }
-//    }
-//}
+function nearPoint(m_x,m_y){
+    var range = 10;
+    var dist = range;
+    var pointIndex;
+    for(i = 0; i < lines.length; i++){
+        // dist to point 1 of line
+        var t1_p1 = lines[i].point1[0] - m_x;
+        var t2_p1 = lines[i].point1[1] - m_y;
+        var pointDist = Math.sqrt(Math.pow((t1_p1),2)+Math.pow((t2_p1),2));
+        
+        if(pointDist < dist){
+            dist = pointDist;
+            selectedPoint.status = true;
+            selectedPoint.lineIndex = i;
+            selectedPoint.point = 1;
+        }
+           
+        // dist to point 2 of line
+        var t1_p2 = lines[i].point2[0] - m_x;
+        var t2_p2 = lines[i].point2[1] - m_y;
+        var pointDist = Math.sqrt(Math.pow((t1_p2),2)+Math.pow((t2_p2),2));
+        
+        if(pointDist < dist){
+            dist = pointDist;
+            selectedPoint.status = true;
+            selectedPoint.lineIndex = i;
+            selectedPoint.point = 2;
+        }
+            
+    }
+    if(dist > range){
+        console.log({dist:dist,rang:range})
+        selectedPoint.status = false;
+    }
+}
 
 
-
+// Calcula os pontos de interseção entre duas retas
 function line_intersect(a,b){
     a.m = (a[0].y-a[1].y)/(a[0].x-a[1].x);  // slope of line 1
     b.m = (b[0].y-b[1].y)/(b[0].x-b[1].x);  // slope of line 2
